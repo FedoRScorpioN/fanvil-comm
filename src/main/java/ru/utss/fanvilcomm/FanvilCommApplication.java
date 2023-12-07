bce4966aff526a54075d2930f87fa3543db8f181
FedoRScorpioN <zapevalov@yandex.ru>
2023-12-07 15:55:08 +0500

    Initial the Project

---
+++ src/main/java/ru/utss/fanvilcomm/FanvilCommApplication.java
@@ -0,0 +1,13 @@
+package ru.utss.fanvilcomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FanvilCommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FanvilCommApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/utss/fanvilcomm/FanvilCommApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.utss.fanvilcomm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FanvilCommApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
